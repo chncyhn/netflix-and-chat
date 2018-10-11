596023d3438bc0c3803efcc81543d5d52bd4775c
Cihan Ceyhan <chncyhn@gmail.com>
2018-10-12 00:03:01 +0200

    Added memoization to rating fetching function.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_caching import Cache
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 from app import routes, models
--- backend/app/rating_helpers.py
+++ backend/app/rating_helpers.py
@@ -1,14 +1,14 @@
 from imdb import IMDb
-
+from app import cache
 
 ia = IMDb()
 
+@cache.memoize()
 def fetch_imdb_rating(track_name):
     """
     Request the rating of a track from IMDB.
     Returns the rating as a string.
     """
-
     movie = ia.search_movie(track_name, results=1)[0]
     movie = ia.get_movie(movie.movieID)
 
